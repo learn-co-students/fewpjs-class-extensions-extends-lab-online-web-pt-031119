b1a31be882ef358f2e38dcd996cbe6eaf9c31d5e
“Mark <mkanko66@gmail.com>
2019-10-24 13:21:07 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+class Polygon {
+
+    constructor(shapeSides) {
+        this.shapeSides = shapeSides 
+    }
+
+    get countSides() {
+        return this.shapeSides.length  
+    }
+
+    get perimeter() {
+        return this.shapeSides.reduce((memo, element) => {return element + memo }, 0) 
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+        const pair1 = this.shapeSides[0] + this.shapeSides[1] 
+        const pair2 = this.shapeSides[1] + this.shapeSides[2] 
+        const pair3 = this.shapeSides[0] + this.shapeSides[2]
+
+        if (pair1 > this.shapeSides[2] && pair2 > this.shapeSides[0] && pair3 > this.shapeSides[1]) {
+            return true   
+        } else {
+            return false 
+        }
+    }
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+        for (let i = 0; i < this.shapeSides.length; i++) {
+            if (this.shapeSides[i] !== this.shapeSides[0]) {
+                return false 
+            }
+        }    
+        return true   
+    }
+    
+    get area() {
+        return this.shapeSides[0] * this.shapeSides[1]         
+    }
+}
+
+
+
+
+
