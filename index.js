acfb2e94a89e4613a9da7a4791109718c53db817
“Mark <mkanko66@gmail.com>
2019-10-24 13:45:52 -0700

    refactor lab

--- index.js
+++ index.js
@@ -16,27 +16,27 @@ class Polygon {
 class Triangle extends Polygon {
 
     get isValid() {
-        const pair1 = this.shapeSides[0] + this.shapeSides[1] 
-        const pair2 = this.shapeSides[1] + this.shapeSides[2] 
-        const pair3 = this.shapeSides[0] + this.shapeSides[2]
+        const pair1 = this.shapeSides[0] + this.shapeSides[1] > this.shapeSides[2] 
+        const pair2 = this.shapeSides[1] + this.shapeSides[2] > this.shapeSides[0]
+        const pair3 = this.shapeSides[0] + this.shapeSides[2] > this.shapeSides[1]
 
-        if (pair1 > this.shapeSides[2] && pair2 > this.shapeSides[0] && pair3 > this.shapeSides[1]) {
-            return true   
-        } else {
-            return false 
-        }
+        return pair1 && pair2 && pair3 
     }
 }
 
 class Square extends Polygon {
 
+    // get isValid() {
+    //     for (let i = 0; i < this.shapeSides.length; i++) {
+    //         if (this.shapeSides[i] !== this.shapeSides[0]) {
+    //             return false 
+    //         }
+    //     }    
+    //     return true   
+    // }
+
     get isValid() {
-        for (let i = 0; i < this.shapeSides.length; i++) {
-            if (this.shapeSides[i] !== this.shapeSides[0]) {
-                return false 
-            }
-        }    
-        return true   
+        return this.shapeSides.every(element => element === this.shapeSides[0]) 
     }
     
     get area() {
